1b535ae1af4bff795883f715e3940e6fac46718d
GeorgeU2030 <georgeu2021@gmail.com>
2024-01-21 20:18:53 -0500

    refactor_the_code

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,6 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   
-  delete '/products/:id', to: 'products#destroy'
-  patch '/products/:id', to: 'products#update'
-  post '/products', to: 'products#create'
-  get '/products/new', to: 'products#new', as: :new_product
-  get '/products', to: 'products#index'
-  get '/products/:id', to: 'products#show', as: :product
-  get '/products/:id/edit', to: 'products#edit', as: :edit_product
+  resources :products, path: '/'
 
 end
